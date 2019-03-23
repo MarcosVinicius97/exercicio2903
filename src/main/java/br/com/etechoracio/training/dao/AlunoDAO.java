686420dc38ecca839372b335514007fbbb57a002
daniella-melo <fcmdaniella@gmail.com>
2019-03-22 22:28:49 -0300

    Ex.13, Ex.14, Ex.15

---
+++ src/main/java/br/com/etechoracio/training/dao/AlunoDAO.java
@@ -0,0 +1,9 @@
+package br.com.etechoracio.training.dao;
+
+import br.com.etechoracio.common.dao.BaseDAO;
+import br.com.etechoracio.training.model.Aluno;
+
+public interface AlunoDAO extends BaseDAO<Aluno> {
+
+		
+}
